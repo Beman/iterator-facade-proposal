3d9cc4565c34e9959554d535286e7ba613144cfd
Beman <bdawes@acm.org>
2015-12-30 09:20:09 -0500

    Revert to be closer to Eric's original ranges-v3 code. Fix failure to change the name of get() to read(), which caused much trouble diagnosing. Add typeid reporting.

--- example1.cpp
+++ example1.cpp
@@ -7,50 +7,54 @@
 //  Boost Software License, Version 1.0.
 //  See http://www.boost.org/LICENSE_1_0.txt)
 
-#include <cstring>
 #include <cassert>
-#include <tuple>
 #include <iostream>
+#include <typeinfo>
 #include <stl2/detail/iterator/basic_iterator.hpp>
-
-//#include <stl2/detail/iterator/counted_iterator.hpp>
 
 namespace stl2 = __stl2;
 
 namespace
 {
-  struct cursor
-  {
-  public:
-    cursor() = default;
-
-    struct mixin : protected stl2::basic_mixin<cursor>
+    template<typename I>
+    struct cursor
     {
-      mixin() = default;
-      using stl2::basic_mixin<cursor>::basic_mixin;
-      mixin(I p) : basic_mixin<cursor>(cursor{p}) {}
+        I it_;
+        struct mixin : stl2::basic_mixin<cursor>
+        {
+            mixin() = default;
+            mixin(cursor c) : stl2::basic_mixin<cursor>(c) {}
+            mixin(I i) : stl2::basic_mixin<cursor>(cursor{i}) {}
+        };
+        cursor() = default;
+        explicit cursor(I i) : it_(i) {}
+//        template<class J, CONCEPT_REQUIRES_(stl2::ConvertibleTo<J, I>())>
+//        cursor(cursor<J> that) : it_(std::move(that.it_)) {}
+//        cursor(cursor that) : it_(std::move(that.it_)) {}
+        auto read() const -> decltype(*it_) { return *it_; }
+        void next() { ++it_; }
     };
 
-    explicit cursor(const char* p) : p_(p) {}
-
-    auto get() const -> decltype(*p_) { return *p_; }
-    void next() { ++p_; }
-
-  private:
-    const char* p_;
-  };
-
-  using iterator = stl2::basic_iterator<cursor>;
+    template<class I>
+    using iterator = stl2::basic_iterator<cursor<I>>;
 
 }  // unnamed namespace
 
 int main()
 {
-  std::cout << "\nTesting basic_iterator\n";
+  std::cout << "Testing basic_iterator" << std::endl;
 
   static const char sz[] = "hello world";
-  iterator itr{sz};
+  typedef iterator<char const*> iterator_t;
+  std::cout << typeid(iterator_t).name() << std::endl;
+
+  iterator_t itr{sz};
+
+  std::cout << typeid(decltype(*itr)).name() << std::endl;
+
   assert(*itr == 'h');
   ++itr;
   assert(*itr == 'e');
+
+  std::cout << "Testing basic_iterator complete" << std::endl;
 }
