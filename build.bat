6a4fc0ce19c5f3bd9e010441a4c1eb0937426ac1
Beman <bdawes@acm.org>
2015-12-30 09:14:02 -0500

    Add help. Change include path.

--- build.bat
+++ build.bat
@@ -1,1 +1,10 @@
-g++ -std=c++1z -fmax-errors=5 -I../cmcstl2/include %*
+@echo off
+if %1$==$ goto help
+g++ -std=c++1z -fmax-errors=5 -I../stl2/include %*
+goto done
+
+:help
+echo Build using g++
+echo Invoke: build [gcc-option...] path...
+:done
+
