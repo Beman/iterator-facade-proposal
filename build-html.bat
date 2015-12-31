64ee9fb73009f905edd0e2fe603e62657c7095cc
Beman <bdawes@acm.org>
2015-12-31 12:04:23 -0500

    Add %!{...}!% to add <em>...</em> tags using Jonathan Wakely's sed trick.

--- build-html.bat
+++ build-html.bat
@@ -1,3 +1,4 @@
 @echo off
-pandoc --css=styles.css --toc --toc-depth=4 -f markdown_github -t html -o iterator-facade-proposal.html iterator-facade-proposal.md
+pandoc --css=styles.css --toc --toc-depth=4 -f markdown_github -t html -o iterator-facade-proposal.temp iterator-facade-proposal.md
+sed "s:%%!{:<em>:g;s:}!%%:</em>:g" <iterator-facade-proposal.temp >iterator-facade-proposal.html
 iterator-facade-proposal.html
