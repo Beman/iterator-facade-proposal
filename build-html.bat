ae1eee164b6b00800cc8a8afcc23f17508e73e00
Beman <bdawes@acm.org>
2016-01-19 17:38:12 -0500

    Switch Table of Contents generation to html_toc, and then use html_include_files to position the TOC correctly in the document.

--- build-html.bat
+++ build-html.bat
@@ -1,6 +1,7 @@
 @echo off
-html_include_files iterator-facade-proposal.md iterator-facade-proposal-0.md
-pandoc --css=styles.css --toc --toc-depth=5 -f markdown_github+pipe_tables -t html -o iterator-facade-proposal.temp iterator-facade-proposal-0.md
-sed "s:%%!{:<em>:g;s:}!%%:</em>:g" <iterator-facade-proposal.temp >iterator-facade-proposal-2.temp
-html_section_numbers --initial=24.8.8 <iterator-facade-proposal-2.temp >iterator-facade-proposal.html
+pandoc --css=styles.css -f markdown_github+pipe_tables -t html -o iterator-facade-proposal.temp iterator-facade-proposal.md
+sed "s:%%!{:<em>:g;s:}!%%:</em>:g" <iterator-facade-proposal.temp >iterator-facade-proposal-1.temp
+html_section_numbers --initial=24.8.8 <iterator-facade-proposal-1.temp >iterator-facade-proposal-2.temp
+html_toc -x iterator-facade-proposal-2.temp toc.html
+html_include_files iterator-facade-proposal-2.temp iterator-facade-proposal.html
 iterator-facade-proposal.html
