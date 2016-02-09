c70d00a7372b7ea68eb1c5b0cb7ac18c7672c6ce
Beman <bdawes@acm.org>
2016-02-09 11:07:49 -0500

    Add --self-contained and --title-prefix options.

--- build-html.bat
+++ build-html.bat
@@ -1,5 +1,5 @@
 @echo off
-pandoc --css=styles.css -f markdown_github+pipe_tables -t html -o iterator-facade-proposal.temp iterator-facade-proposal.md
+pandoc --self-contained --css=styles.css --title-prefix="Iterator Facade" -f markdown_github+pipe_tables -t html -o iterator-facade-proposal.temp iterator-facade-proposal.md
 sed "s:%%!{:<em>:g;s:}!%%:</em>:g" <iterator-facade-proposal.temp >iterator-facade-proposal-1.temp
 html_section_numbers --initial=24.8.8 <iterator-facade-proposal-1.temp >iterator-facade-proposal-2.temp
 html_toc -x iterator-facade-proposal-2.temp toc.html
